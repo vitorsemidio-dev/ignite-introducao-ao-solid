9931c8604598ea6d5190ca9dba01e07529f43ae2
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-08-23 03:46:58 -0300

    feat: Implementação

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,30 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = Object.assign(new User(), { name, email });
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const index = this.users.indexOf(receivedUser);
+    if (index < 0) {
+      throw new Error("User not found");
+    }
+    this.users[index].admin = true;
+    return this.users[index];
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
